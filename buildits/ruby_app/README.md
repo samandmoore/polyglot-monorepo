3b0c4676b68bd7ac220d0666e5a78edb29e20668
Sam Moore <sam@betterment.com>
2017-08-09 10:55:55 -0400

    move files

---
+++ buildits/ruby_app/README.md
@@ -0,0 +1,9 @@
+# Buildit for building a ruby app
+
+this directory provides a set of files for properly building a ruby app
+for deployment as a container.
+
+it will:
+
+* run `bundle exec rake`
+* build a container for the ruby app
--- buildits/ruby_app/build.sh
+++ buildits/ruby_app/build.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
+THIS_DIR="$(dirname $0)"
+
 DOCKERFILE_NAME="Dockerfile.generated"
 APP_NAME="$1"
 IMAGE_NAME="polyglot-monorepo/$APP_NAME"
 
-./make_dockerfile.rb "$APP_NAME" > "$DOCKERFILE_NAME"
+./$THIS_DIR/make_dockerfile.rb "$APP_NAME" > "$DOCKERFILE_NAME"
+
 docker build -t "$IMAGE_NAME" -f "$DOCKERFILE_NAME" .
