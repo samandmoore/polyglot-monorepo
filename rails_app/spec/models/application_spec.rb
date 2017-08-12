f5fa4a10d410f952b195d4cd42cc20b093af81b4
Sam Moore <sam@betterment.com>
2017-08-12 13:38:39 -0400

    sample model specs with pass and fail

---
+++ rails_app/spec/models/application_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe Application, type: :model do
+  it 'works' do
+    expect(true).to eq true
+  end
+
+  it 'fails' do
+    expect(true).to eq false
+  end
+end
